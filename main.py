204f4979b339ad304076bc5488957117640a58e3
Luis Chavarriaga <luischa123@gmail.com>
2020-10-13 14:53:25 -0500

    initial commit
    commit inicial del repositorio. Ubica la carpeta del dataset en
    el directorio base de este repositorio.

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import xml.etree.ElementTree as xml
+from sklearn.pipeline import make_pipeline
+
+# importing the dataset
+users = []
+for file_
