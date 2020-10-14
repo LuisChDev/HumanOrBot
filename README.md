11119c28daff7d3321736401d4db181a81ece4c7
Luis Chavarriaga <luischa123@gmail.com>
2020-10-13 19:28:20 -0500

    changes
    algunos cambios sin significancia.

---
+++ README.md
@@ -0,0 +1,4 @@
+# HumanOrBot
+
+Este proyecto busca desarrollar un modelo predictivo para determinar si
+determinados usuarios de redes sociales son realmente humanos, o programas.
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import os
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
 import xml.etree.ElementTree as xml
-from sklearn.pipeline import make_pipeline
+# from sklearn.pipeline import make_pipeline
 
 # importing the dataset
+# directorios
+dirdata = 'pan19_author_profiling_training_es/'
+dirname = os.path.abspath('.')
+
 users = []
-for file_
+for file_ in ["".join([dirname, '/', dirdata, x]) for x in os.listdir(
+        os.path.join(dirname, dirdata))]:
+    xml.parse(file_)
+    # print(file_)
